3cd20007bf80ff79a43832a500b0230bcbbbf0d7
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-10-09 00:04:32 +0630

    first prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from "react";
+import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
+import { geolocated } from "react-geolocated";
+import "./App.css";
+const DEFAULT_LATITUDE = 0;
+const DEFAULT_LANGITUDE = 0;
+class App extends React.Component {
+  render() {
+    return !this.props.isGeolocationAvailable ? (
+      <div className="center">Your browser does not support Geolocation</div>
+    ) : !this.props.isGeolocationEnabled ? (
+      <div className="center">Geolocation is not enabled</div>
+    ) : this.props.coords ? (
+      <MapContainer
+        center={[this.props.coords.latitude, this.props.coords.longitude]}
+        zoom={2}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[this.props.coords.latitude, this.props.coords.longitude]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Popup>I am here.</Popup>
+        </Marker>
+      </MapContainer>
+    ) : (
+      <div className="center"> Loading.. </div>
+    );
+  }
 }
 
-export default App;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
